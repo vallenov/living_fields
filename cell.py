e30967b770aae9ffde333a94221429babeddf4a7
vallenov <a2lenov@mail.ru>
2022-12-01 00:37:50 +0300

    Добавлены первые наработки

---
+++ cell.py
@@ -0,0 +1,22 @@
+
+SPRITE = {
+    'empty': ' ',
+    'wall': 'П',
+    'life': '0',
+    'food': '.'
+}
+
+
+class BaseCell:
+    def __init__(self, position: tuple):
+        self.position = position
+        self.look = SPRITE['empty']
+        self.life_forms = []
+
+    def __repr__(self):
+        return f'{self.__class__}'
+
+    def __str__(self):
+        if self.life_forms:
+            self.look = SPRITE['life']
+        return self.look
