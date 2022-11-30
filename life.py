4651605362b2ce490116d406963c1fdf1eb1703e
vallenov <a2lenov@mail.ru>
2022-12-01 00:38:18 +0300

    Добавлены первые наработки

---
+++ life.py
@@ -0,0 +1,10 @@
+from genotype import Genotype
+
+
+class Life:
+    def __init__(self):
+        self.gen = Genotype()
+
+    def born(self):
+        new_life = Life()
+        new_life.gen = self.gen.mutation()
