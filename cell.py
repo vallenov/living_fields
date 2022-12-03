171da91960e8141a57a10fe729379ffa6d1e0017
vallenov <a2lenov@mail.ru>
2022-12-04 01:45:26 +0300

    Добавлен метод движения

--- cell.py
+++ cell.py
@@ -1,3 +1,4 @@
+import random
 from typing import Optional
 from life import Life
 
@@ -14,13 +15,18 @@ class BaseCell:
     def __init__(self, position: tuple):
         self.position: Optional[tuple] = position
         self.look: Optional[str] = SPRITE['empty']
-        self.life_forms: Optional[Life] = None
+        self.life_form: Optional[Life] = None
         self.neighbours: Optional[list] = []
 
     def __repr__(self):
         return f'{self.__class__}'
 
     def __str__(self):
-        if self.life_forms:
+        if self.life_form:
             self.look = SPRITE['life']
         return self.look
+
+    def move(self):
+        rand_neighbour = random.choice(self.neighbours)
+        rand_neighbour.life_form = self.life_form
+        self.life_form = None
